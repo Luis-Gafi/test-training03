3710e2c74e97263140497d80af90925924434c31
Luis Garelli <luiscarlosgarelli@gmail.com>
2024-08-19 21:03:13 +0200

    refactorizacion de codigo para agregar funciones

--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -1,41 +1,34 @@
 import { test, expect } from '@playwright/test';
 
-//Go to URL
-test('Buscar con la lupa', async ({ page }) => {;
+// Función para navegar a la página y aceptar cookies
+async function navigateAndAcceptCookies(page) {
     await page.goto('https://www.amazon.es');
-
-    // Accept cookies
     await page.locator('#sp-cc-accept').click();
+}
 
-    // Write
-    await page.locator('#twotabsearchtextbox').fill('Playstation 5');
+// Función para buscar un término en la barra de búsqueda
+async function searchForItem(page, item) {
+    await page.locator('#twotabsearchtextbox').fill(item);
+}
 
-    // Press key
-
-    await page.keyboard.press('Enter');
-
-    // View results
+// Función para verificar que los resultados de la búsqueda son visibles
+async function verifySearchResultsVisible(page) {
     await expect(page.locator('#search')).toBeVisible();
-    
-    // img.s-image
+}
+
+// Prueba de buscar con la lupa
+test('Buscar con la lupa', async ({ page }) => {
+    await navigateAndAcceptCookies(page);
+    await searchForItem(page, 'Playstation 5');
+    await page.keyboard.press('Enter');
+    await verifySearchResultsVisible(page);
     await page.locator('img.s-image').nth(2).click();
 });
 
-test.skip('Buscar con enter', async ({ page }) => {;
-    await page.goto('https://www.amazon.es');
-        
-    // Accept cookies
-    await page.locator('#sp-cc-accept').click();
-        
-    // Write
-    await page.locator('#twotabsearchtextbox').fill('Playstation 5');
-        
-    // Click  MagGlass
-    
+// Prueba de buscar con Enter
+test.skip('Buscar con enter', async ({ page }) => {
+    await navigateAndAcceptCookies(page);
+    await searchForItem(page, 'Playstation 5');
     await page.locator('.nav-input[type="submit"]').click();
-    
-    // View results
-    await expect(page.locator('#search')).toBeVisible();
-    
-        
-    });
+    await verifySearchResultsVisible(page);
+});
